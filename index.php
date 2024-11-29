979a892796dc3d531cd23ccd8ec1eb206e45f75b
Your Name <you@example.com>
2024-11-29 15:02:29 +0100

    changed button label on index page

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
         <div class="logo">Bandpage Nelson Oliver & The Unusual Suspects</div>
         <ul class="nav-links">
              
-             <li><a href="about.php">Home</a></li>
+             <li><a href="about.php">Band</a></li>
              <li><a href="keys.php">Arne's Electronics</a></li>
              <li><a href="login.php">Login</a></li>
          </ul>
